7a508b2f97bd8f606cc418b7c9a2eeeba7e13eeb
Sonam Tandin Wang <sonamtandinwang@gmail.com>
2017-05-04 16:56:56 -0400

    lets go live and see what happens

---
+++ app.js
@@ -0,0 +1,17 @@
+
+function openDueDatesCaseStudy() {
+    document.getElementById("release-and-due-dates").style.left="0%";    
+}
+
+function openFacultyExperienceRevision() {
+    document.getElementById("faculty-experience-revision").style.left="0%";    
+}
+
+function closeReleaseAndDueDates() {
+    document.getElementById("release-and-due-dates").style.left = "100%";
+}
+
+function closFacultyExperienceRevision() {
+    document.getElementById("faculty-experience-revision").style.left = "100%";
+}
+
